8f10c39923b66cdd9ee24be32a3373bbf9b4122a
Bob Ruiken <bobruiken@gmail.com>
2020-05-03 12:29:42 +0200

    Fixed a number of issues in the prettyprinter
    also some minor restructuring

--- nonogramsolver/board/prettyprint.py
+++ nonogramsolver/board/prettyprint.py
@@ -4,31 +4,49 @@ from nonogramsolver.board.board import Board
 class PrintBoard:
     def __init__(self, board):
         self.board = board
+        self.col_consts = [list(reversed(x)) for x in self.board.get_col_constraints()]
+        self.row_consts = self.board.get_row_constraints()
+        self.max_row_const = max(len(x) for x in self.row_consts)
+        self.max_col_const = max(len(x) for x in self.col_consts)
 
     def print(self):
-        col_consts = self.board.get_col_constraints()
-        row_consts = self.board.get_row_constraints()
-        max_row_const = max(len(x) for x in row_consts)
-        max_col_const = max(len(x) for x in col_consts)
+        """
+        Print the board to the console.
+        """
+        print(self._print())
+    def _print(self):
+        result = self._get_col_constraint_str()
+        result += ' ' * (2 * self.max_row_const + 1) + '=' * (2 * self.board.get_width()) + '\n'
+        result += self._get_rows_str()
+        return result
         result = ''
-        for cc_idx in range(max_col_const):
-            result += ' ' * (2 * max_row_const + 2)
+        for cc_idx in range(self.max_col_const):
+            result += ' ' * (2 * self.max_row_const + 2)
             for col in range(self.board.get_width()):
-                if len(col_consts[col]) >= max_col_const - cc_idx:
-                    result += str(col_consts[col][len(col_consts[col]) - cc_idx - 1]) + ' '
+                if len(self.col_consts[col]) >= self.max_col_const - cc_idx:
+                    result += str(self.col_consts[col][self.max_col_const - cc_idx - 1]) + ' '
                 else:
                     result += '  '
             result += '\n'
-        result += ' ' * (2 * max_row_const + 1) + '=' * (2 * self.board.get_width()) + '\n'
+        return result
+
+    def _get_rows_str(self):
+        result = ''
         for row in range(self.board.get_height()):
-            result += '  ' * (max_row_const - len(row_consts[row])) + ' '.join(str(x) for x in row_consts[row]) + ' ǁ'
-            for v in self.board.get_row(row):
+            result += '  ' * (self.max_row_const - len(self.row_consts[row])) + \
+                      ' '.join(str(x) for x in self.row_consts[row]) + \
+                      ' ' * (min(1, len(self.row_consts[row]))) + 'ǁ'
+            result += self._get_row_str(row)
+        return result
+
+    def _get_row_str(self, row_idx):
+        result = ''
+        for v in self.board.get_row(row_idx):
+            result += ' '
+            if v == Board.Empty:
                 result += ' '
-                if v == Board.Empty:
-                    result += ' '
-                elif v == Board.Unknown:
-                    result += '?'
-                elif v == Board.Cross:
-                    result += 'X'
-            result += '\n'
-        print(result)
+            elif v == Board.Unknown:
+                result += '?'
+            elif v == Board.Cross:
+                result += 'X'
+        return result + '\n'
