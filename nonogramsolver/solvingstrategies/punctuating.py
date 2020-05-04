4259637f300c556ebc17cc7e7db510ff4d83abc5
Bob Ruiken <bobruiken@gmail.com>
2020-05-04 21:09:45 +0200

    Deleted the punctuating strategy as it is empty as of yet

--- nonogramsolver/solvingstrategies/punctuating.py
+++
@@ -1,9 +0,0 @@
-from nonogramsolver.solvingstrategies.basestrategy import BaseStrategy
-
-
-class Punctuating(BaseStrategy):
-    def __init__(self, board):
-        super().__init__(board)
-
-    def apply_strategy(self, values, constraints):
-        pass
