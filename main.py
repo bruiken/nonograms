caf4e466a2149f36ba3558c3c5239cecbd1ff1ec
Bob Ruiken <bobruiken@gmail.com>
2020-07-25 21:14:23 +0200

    Added examples folder

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from nonogramsolver.board import BoardReader, PrintBoard
-from nonogramsolver.solvingstrategies import SimpleBoxes, SimpleSpaces
-
+from nonogramsolver.solvingstrategies import SimpleBoxes
 
 if __name__ == '__main__':
-    b = BoardReader('exampleboard.json').get_board()
+    b = BoardReader('examples/exampleboard.json').get_board()
     PrintBoard(b).print()
     SimpleBoxes(b).apply()
-    PrintBoard(b).print()
-    SimpleSpaces(b).apply()
     PrintBoard(b).print()
